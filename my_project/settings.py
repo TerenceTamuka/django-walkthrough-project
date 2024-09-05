10f2117b011b60eef2bc975f13b5e1d193c99c72
Terence Zengeya <terencetamuka@gmail.com>
2024-09-05 11:47:53 +0000

    Added heroku configurations for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ps19lg!#_r92lf$v)of1w!c$4fl_i8iu!km7u0^j8ov#97fxp0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-terencetamu-djangowalkt-db463og0ahw.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-terencetamu-djangowalkt-db463og0ahw.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
